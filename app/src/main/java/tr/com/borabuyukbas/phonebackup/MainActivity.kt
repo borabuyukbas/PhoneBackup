f4ae6602d9908dfc266b29b08159a0cd6110e43f
Bora Büyükbaş <borabuyukbas@hotmail.com>
2023-11-24 23:36:49 +0300

    chore: upgrade target SDK and implementations

--- app/src/main/java/tr/com/borabuyukbas/phonebackup/MainActivity.kt
+++ app/src/main/java/tr/com/borabuyukbas/phonebackup/MainActivity.kt
@@ -3,65 +3,13 @@ package tr.com.borabuyukbas.phonebackup
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import tr.com.borabuyukbas.phonebackup.components.Header
-import tr.com.borabuyukbas.phonebackup.components.Navbar
-import tr.com.borabuyukbas.phonebackup.ui.theme.PhoneBackupTheme
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            PhoneBackupTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Scaffold(
-                        topBar = {
-                            Header()
-                        },
-                        bottomBar = {
-                            Navbar()
-                        }
-                    ) { paddingValues ->
-                        Column(
-                            modifier = Modifier.padding(paddingValues)
-                        ) {
-                            Greeting("Android")
-                        }
-                    }
-                }
-            }
+            Application()
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PhoneBackupTheme {
-        Greeting("Android")
     }
 }
